fe08e0a7e36c1af53b156b7d30f6cbfe478b7657
Seikened <fernanlee2131@gmail.com>
2021-10-08 22:27:18 -0500

    Create Estructura de control.py

---
+++ Modulo_I/Estructura de control.py
@@ -0,0 +1,20 @@
+print("Me voy a la cocina")
+print("Abro el refri y...")
+hay_leche=input("¿Hay leche? (S/N)")
+hay_chocomilk=input("¿Hay chocomilk? (S/N)")
+
+if hay_leche!="S" or hay_chocomilk!="S":
+    print("Voy a al super a comprar...")
+    if hay_leche!="S":
+        print(" leche")
+    if hay_chocomilk!="S":
+        print(" chocomilk")
+
+
+
+
+    print("Sacamos la leche")
+    print("Sacamos el chocomilk")
+    print("Ponemos la leche en el vaso")
+    print("Ponemos el chocomilk en el vaso")
+    print("Se mezclan")
