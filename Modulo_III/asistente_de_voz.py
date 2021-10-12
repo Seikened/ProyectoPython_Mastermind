02a1c65f2cf754f8932f8be2824c5713eef9fea5
Seikened <fernanlee2131@gmail.com>
2021-10-12 17:12:49 -0500

    all

--- Modulo_III/asistente_de_voz.py
+++ Modulo_III/asistente_de_voz.py
@@ -1,6 +1,6 @@
-import pyttsx3
-import speech_recognition as sr
 import re
+from speak_and_listen import speak,listen
+
 
 
 
@@ -9,40 +9,22 @@ def identify_name(text):
     name = None
     patterns = ["me llamo ([A-Za-z]+)", "yo soy ([A-Za-z]+)", "^([A-Za-z]+)$"]
     #for pattern in patterns: 
-        
-    name = re.findall("me llamo ([A-Za-z]+)", text)[0]
-    print(name)
-     
-          
+    for pattern in patterns:
+        try:
+            name = re.findall(pattern, text)[0]
+            print(name)
+        except IndexError:
+            pass   
     return name
 
 
 
 
 def main():
-    engine = pyttsx3.init()
-    engine.setProperty("rate",120)
-    engine.setProperty("vioice", "spanish")
+    speak("Hola ")
+    text = listen()
 
-
-    engine.say("Hola, ¿cómo te llamas?")
-    engine.runAndWait()
-
-    r = sr.Recognizer() #Se encarga de recojer información si reconoce algun tipo de habla...
-
-    with sr.Microphone() as source:
-        print("Puedes hablar")
-        audio = r.listen(source)
-        text = r.recognize_google(audio, language="es=MX")
-        print(text+"\n")
-        
-        name = identify_name(text)
-
-        if name:
-            engine.say("Encantado de conocerte {}".format(name))
-        else:   
-            engine.say("No te entendi, vamos otra vez")
-        engine.runAndWait()
+    
 
 
 
---
+++ Modulo_III/speak_and_listen.py
@@ -0,0 +1,35 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+
+
+
+engine = pyttsx3.init()
+engine.setProperty("rate",120)
+engine.setProperty("voice", "spanish")
+
+r = sr.Recognizer()#Este es nuestro reconocerdor
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+def listen():
+    with sr.Microphone() as source:   #Aqui se abre y empieza a tener el microfono como un recurso y empieza a almacenar el texto
+        print("Escuchando... ")
+        audio = r.listen(source)   #Aquí se deja todo el recurso de lo de arriba
+
+        try:
+            text = r.recognize_google(audio, language="es=MX")
+            print("he entendido: {}".format(text))
+            return text
+        except sr.UnknownValueError:
+            print("Lo siento pero no te he entendido")
+            
+
+
+
+
