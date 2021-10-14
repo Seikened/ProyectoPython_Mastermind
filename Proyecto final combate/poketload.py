a3f9bd8a502083c8baf13de88ceff2c7375ba80a
Seikened <fernanlee2131@gmail.com>
2021-10-14 18:08:18 -0500

    as

--- Proyecto final combate/poketload.py
+++ Proyecto final combate/poketload.py
@@ -1,22 +1,20 @@
 from requests_html import HTMLSession
-import re
+import re, random
 
 
 pokemon_base = {
+    "url_photo":"",
     "name": "",
     "current_health": 100,
     "base_health": 100,
     "level": 1,
+    "type": [],
     "current_exp": 0
 
 }
-
-URL_BASE = "https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/movimientos_nivel&pk="
-
-"""for items in chrome_history:
-        results = re.findall("https://www.instagram.com/([A-Za-z0-9_.-]+)/+$", items[2])
-        if results and results[0] not in ["fer_leonfranco"]:  # FILTRO
-            profiles_visited.append(results[0])"""
+MAIN_URL = "https://www.pokexperto.net/"
+URL_BASE = "{}index2.php?seccion=nds/nationaldex/movimientos_nivel&pk=".format(MAIN_URL)
+index = random.randrange(1,150)
 
 
 def get_pokemon(index):
@@ -25,17 +23,25 @@ def get_pokemon(index):
     url_img_pokemon = "https://www.pokexperto.net/nds/artwork/{}.jpg".format(index)
 
     
-   
+    new_pokemon = pokemon_base.copy()
     pokemon_page = session.get(url)
-    pokemon_name = pokemon_page.html.find(".mini", first=True).text
-    pokemon_type_url_image = pokemon_page.html.find(".bordeambos", first=True)
-    pokemon_type = str(re.findall("https://www.pokexperto.net/3ds/sprites/tipos/([A-Za-z]+).png",pokemon_type_url_image)).title()
-    
-    
-    print(pokemon_type)
-    print(pokemon_name)
+    pokemon_type_search= pokemon_page.html.find(".bordeambos", first=True).html
+
+    new_pokemon["url_photo"] = url_img_pokemon
+    new_pokemon["name"] = pokemon_page.html.find(".mini", first=True).text
+    #current_health
+    #base_heatl
+    #level
+    new_pokemon["type"]= re.findall("3ds/sprites/tipos/([A-Za-z]+).png",pokemon_type_search)
+    #current_exp
+
+    print("\n\nNumero de Pokemon #{}".format(index))
+    print(new_pokemon["url_photo"])
+    print("\n",url)
+    print((" ".join(new_pokemon["type"]).title()))
+    print(new_pokemon["name"],"\n")
 
 
 
 
-get_pokemon(1)
+get_pokemon(index)
