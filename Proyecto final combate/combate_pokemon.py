74c813abe27c46b055174fdd5438c0729473f78c
Seikened <fernanlee2131@gmail.com>
2021-10-16 11:27:11 -0500

    a

--- Proyecto final combate/combate_pokemon.py
+++ Proyecto final combate/combate_pokemon.py
@@ -20,12 +20,35 @@ def get_player_profile(pokemon_list):
     return  { 
         "player_name": input("¿Cual es tu nombre? "),
         "pokemon_inventory": [random.choice(pokemon_list) for a in range(3)], 
+        "combats":0,
         "pokeballs": 0,
         "heat_potion": 0,
-        "combats":0
     }                                                                               
 
                                                                                     
+def any_player_pokemon_lives(player_profile):
+   return sum( [pokemon["current_health"] for pokemon in player_profile["pokemon_inventory"]]) > 0 # Si la suma de todas las vidas de los pokemon es mayor a 0 devolvera un True si no un False
+
+
+def fight(player_profile,enemy_pokemon):
+    #ENEMY POKEMON
+    select_attack = random.choice(enemy_pokemon["attacks"])
+    ##Min_Level
+    if select_attack["min_livel"] > enemy_pokemon["level"]:
+        select_attack = random.choice(enemy_pokemon["attacks"])
+    enemy_damage = select_attack["damage"]
+    enemy_name_attack = select_attack["name"]
+    enemy_type_attack = select_attack["type"]
+    ####################################################################
+
+
+
+
+
+
+
+
+
 
 
 
@@ -33,6 +56,12 @@ def main():
     pokemon_list = get_all_pokemons()
     player_profile = get_player_profile(pokemon_list)
     pprint(player_profile)
+
+    while any_player_pokemon_lives(player_profile):
+        enemy_pokemon = random.choice(pokemon_list)
+        fight(player_profile,enemy_pokemon)
+        
+    print("Has perdido en el combate numero {}".format(player_profile["combats"]))
 
 if __name__ == "__main__":
     main()
